86175768cdfbc9696d35bee640f0725a26791a25
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-19 23:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function insert_Row() {
     //Write your code here
-  
+  const table = document.getElementById("sampleTable");
+
+    // 2. Create a new row
+    const newRow = table.insertRow(0);
+
+    // 3. Create new cells and add text
+    const nameCell = document.createElement("td");
+    nameCell.textContent = "New Cell1";
+
+    const ageCell = document.createElement("td");
+    ageCell.textContent = "New Cell2";
+
+    // 4. Append cells to the row
+    newRow.appendChild(nameCell);
+    newRow.appendChild(ageCell);
+
+    // 5. Append the row to the table
+   
   
 }
